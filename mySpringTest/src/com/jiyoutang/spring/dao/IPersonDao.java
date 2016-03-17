5069c1c51beb911f0782416567bc4d833824ee14
liulong3712 <liulong3712@126.com>
2016-03-17 22:50:54 +0800

    add myspringtest

---
+++ mySpringTest/src/com/jiyoutang/spring/dao/IPersonDao.java
@@ -0,0 +1,17 @@
+package com.jiyoutang.spring.dao;
+
+import java.util.List;
+
+public interface IPersonDao {
+	public String getPersonName(Integer id);
+
+	public void addPerson(Person person);
+
+	public int getPersonsCount();
+
+	public List<Person> listPersons();
+
+	public List findAllPersons();
+	
+	public void testTransactions();
+}
---
+++ mySpringTest/src/com/jiyoutang/spring/dao/Person.java
@@ -0,0 +1,43 @@
+package com.jiyoutang.spring.dao;
+
+import java.util.Date;
+
+
+public class Person {
+
+	private Integer id;
+	private String name;
+	private String sex;
+	private int age;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	private Date birthday;
+}
